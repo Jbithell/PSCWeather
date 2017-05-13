df4cb21f0db6b22d16952b52bf1fe97126cc5776
James Bithell <hi@jbithell.com>
2017-05-13 19:16:38 +0100

    Add some more content to have a quick test

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hi!")
+import os
+import serial
+serialport = os.environ.get('serialport', '/dev/serial0')
+baudrate = os.environ.get('baudrate', 19200)
+ser = serial.Serial(serialport,baudrate)
+ser.open()
+ser.write("LOOP 1" + "\n")
+print(ser.name)
