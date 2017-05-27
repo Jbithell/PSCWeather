a694ddca994a81e096cb691bef9896a204e6d5a9
James Bithell <hi@jbithell.com>
2017-05-27 10:51:34 +0100

    Correct errors in error handling

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ def log(message):
 
 serialport = os.environ.get('serialPort', '/dev/serial0')
 baudrate = os.environ.get('baudRate', 19200) #Set the Baudrate to 19200 which is a nice default for the davis logger
-ser = serial.Serial(serialport, baudrate) #Open a serial connection
 try:
+    ser = serial.Serial(serialport, baudrate)  # Open a serial connection
     ser.open()
 except:
     log("Cannot find device in serial port or open a connection")
-    if (os.getenv('rebootOnSerialFail', False)):
+    if (os.getenv('rebootOnSerialFail', True)):
         os.system("shutdown /r")  # Reboot the device if cannot connect to serial port - ie have a second attempt
 
 ser.write("LOOP 1" + "\n")
+print(ser.readline())
 print(ser.name)
