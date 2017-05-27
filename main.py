f08eb27b7be9541b2f53c8b01359d8b1994e7cf2
James Bithell <hi@jbithell.com>
2017-05-27 10:47:42 +0100

    Camel case a relevant variable (rebootOnSerialFail)

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import serial
 def log(message):
     print(message)
 
-serialport = os.environ.get('serialport', '/dev/serial0')
-baudrate = os.environ.get('baudrate', 19200) #Set the Baudrate to 19200 which is a nice default for the davis logger
+serialport = os.environ.get('serialPort', '/dev/serial0')
+baudrate = os.environ.get('baudRate', 19200) #Set the Baudrate to 19200 which is a nice default for the davis logger
 ser = serial.Serial(serialport, baudrate) #Open a serial connection
 try:
     ser.open()
 except:
     log("Cannot find device in serial port or open a connection")
-    if (os.getenv('REBOOT_ON_SERIAL_FAIL', False)):
+    if (os.getenv('rebootOnSerialFail', False)):
         os.system("shutdown /r")  # Reboot the device if cannot connect to serial port - ie have a second attempt
 
 ser.write("LOOP 1" + "\n")
