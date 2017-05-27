0b184307b80a83b0df12b58ffa2c82d8c4513ca7
James Bithell <hi@jbithell.com>
2017-05-27 10:46:58 +0100

    Add some basic error handling and the REBOOT_ON_SERIAL_FAIL config to prevent a reboot loop

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
 import serial
+def log(message):
+    print(message)
+
 serialport = os.environ.get('serialport', '/dev/serial0')
-baudrate = os.environ.get('baudrate', 19200)
-ser = serial.Serial(serialport,baudrate)
-ser.open()
+baudrate = os.environ.get('baudrate', 19200) #Set the Baudrate to 19200 which is a nice default for the davis logger
+ser = serial.Serial(serialport, baudrate) #Open a serial connection
+try:
+    ser.open()
+except:
+    log("Cannot find device in serial port or open a connection")
+    if (os.getenv('REBOOT_ON_SERIAL_FAIL', False)):
+        os.system("shutdown /r")  # Reboot the device if cannot connect to serial port - ie have a second attempt
+
 ser.write("LOOP 1" + "\n")
 print(ser.name)
