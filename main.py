79567200adcceb3bc24292f963e8ad0ab9f9b405
James Bithell <hi@jbithell.com>
2017-05-28 17:59:33 +0100

    Try again with a mega tidy up

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import serial
 def log(message):
-    print(message)
+    log(message)
 
 serialport = os.environ.get('serialPort', '/dev/ttyUSB0')
 baudrate = os.environ.get('baudRate', 19200) #Set the Baudrate to 19200 which is a nice default for the davis logger
@@ -9,23 +9,27 @@ try:
     ser = serial.Serial(serialport, baudrate, timeout=2)  # Open a serial connection
     ser.isOpen()
 except Exception as e:
-    print(e)
-    log("Cannot find device in serial port or open a connection")
+    log(e)
+    log("[ERROR] Cannot find device in serial port or open a connection")
     if (os.getenv('rebootOnSerialFail', "True") == "True"):
+        log("[INFO] Rebooting")
         os.system("reboot")  # Reboot the device if cannot connect to serial port - ie have a second attempt
 
-
-print ("Sending " + str("\n"))
+log("[INFO] Opening a connection to the weather station")
 ser.write(bytes(str("\n"), 'utf8'))
-print("Sent ")
-print(bytes(str("\n"), 'utf8'))
+if (ser.readline() != "\n"):
+    log("[ERROR] Error getting connection - trying again")
+    ser.write(bytes(str("\n"), 'utf8'))
+    if (ser.readline() != "\n"):
+        log("[ERROR] Error getting connection - rebooting if setting is set")
+        if (os.getenv('rebootOnSerialFail', "True") == "True"):
+            log("[INFO] Rebooting")
+            os.system("reboot")  # Reboot the device if cannot connect to serial port - ie have a second attempt
 
+log("[INFO] Ready to start getting data")
 
 print ("Sending " + str("LOOP 1 \n"))
 ser.write(bytes(str("LOOP 1 \n"), 'utf8'))
-print("Sent ")
-print(bytes(str("LOOP 1 \n"), 'utf8'))
-print(ser.readline()) #Read this line but ignore it - it is just some info to tell you all is well
 response = ser.readline()
 print(response)
 data = {}
@@ -35,4 +39,4 @@ data["winddirection"] = response[16]
 print(data)
 ser.readline() #Read this line but ignore it - it is boring data we don't want
 
-print("Program done")
+log("Program done")
