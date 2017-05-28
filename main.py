96d54893f5b48c7420a8aa960a20523ba5d5ca12
James Bithell <hi@jbithell.com>
2017-05-28 14:28:14 +0100

    Check for errors

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import serial
 def log(message):
     print(message)
 
-serialport = os.environ.get('serialPort', '/dev/ttyS0')
+serialport = os.environ.get('serialPort', '/dev/ttyUSB0')
 baudrate = os.environ.get('baudRate', 19200) #Set the Baudrate to 19200 which is a nice default for the davis logger
 try:
     ser = serial.Serial(serialport, baudrate)  # Open a serial connection
     ser.open()
-except:
+except Exception as e:
+    print(e)
     log("Cannot find device in serial port or open a connection")
     if (os.getenv('rebootOnSerialFail', "True") == "True"):
         os.system("reboot")  # Reboot the device if cannot connect to serial port - ie have a second attempt
