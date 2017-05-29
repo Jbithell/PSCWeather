97b8d0cd170ca37542f2d9c9fec5b2fa647b63d3
James Bithell <hi@jbithell.com>
2017-05-29 14:16:18 +0100

    Check for key error

--- main.py
+++ main.py
@@ -91,6 +91,7 @@ while True:
             requestPayload = urllib.parse.urlencode(data).encode("utf-8")
             requestResponse = urllib.request.urlopen(os.environ.get('uploadUrl', ''), requestPayload)
             requestParsedResponse = json.loads(requestResponse.read().decode('utf-8'))
+            print(requestParsedResponse)
             if (requestParsedResponse["sucess"] != "true"):
                 log("[ERROR] Couldn't upload the data online - server rejected with " + str(requestParsedResponse["message"]))
                 storefailedrequest(data)
