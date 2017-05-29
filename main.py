8577cb76786883d2ced1d187bbc4acaa22c61277
James Bithell <hi@jbithell.com>
2017-05-29 14:10:40 +0100

    Try decoding the response yet again

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ while True:
         try:
             requestPayload = urllib.parse.urlencode(data).encode("utf-8")
             requestResponse = urllib.request.urlopen(os.environ.get('uploadUrl', ''), requestPayload)
-            requestParsedResponse = json.load(requestResponse)
+            requestParsedResponse = json.loads(requestResponse.read().decode('utf-8'))
             if (requestParsedResponse["sucess"] != "true"):
                 log("[ERROR] Couldn't upload the data online - server rejected with " + str(requestParsedResponse["message"]))
                 storefailedrequest(data)
