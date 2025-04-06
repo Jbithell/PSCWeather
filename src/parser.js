49f0a73ea5393600f7d9d0cc32d0ba6a3f5e616b
James Bithell <Jbithell@users.noreply.github.com>
2025-04-06 18:59:22 +0100

    Grab a bit more data

--- src/parser.js
+++ src/parser.js
@@ -5,39 +5,49 @@ const logger = require("./logger");
  */
 const parser = (inputBuffer, offset) => {
   const data = {
-    temperatureRaw: inputBuffer.readUInt16LE(offset + 12), // In degrees F multiplied by 10
-    windDirection: inputBuffer.readUInt16LE(offset + 16), //In degrees
+    barTrend: inputBuffer.readInt8(offset + 3), // current 3-hour barometer trend
     barometer: inputBuffer.readUInt16LE(offset + 7), // In Hg/1000
+    temperatureF: (inputBuffer.readUInt16LE(offset + 12) / 10).toFixed(1), // In degrees F multiplied by 10
     windSpeed: inputBuffer.readUInt8(offset + 14).toFixed(1), // in MPH
+    windDirection: inputBuffer.readUInt16LE(offset + 16), //In degrees
     wind10MinAverage: (inputBuffer.readUInt16LE(offset + 18) / 10).toFixed(1), // in MPH - an average of the last 10 minutes
     wind2MinAverage: (inputBuffer.readUInt16LE(offset + 20) / 10).toFixed(1), // in MPH - an average of the last 2 minutes
     windGust: (inputBuffer.readUInt16LE(offset + 22) / 10).toFixed(1), // in MPH - gust of the last 10 minutes
+    windGustDirection: (inputBuffer.readUInt16LE(offset + 24) / 10).toFixed(1), // in MPH - gust of the last 10 minutes
+    dewPoint: inputBuffer.readInt16LE(offset + 30), // In Degrees F (255 = unavailable)
     humidity: inputBuffer.readUInt8(offset + 33), // in %
+    rainRate: (inputBuffer.readUInt16LE(offset + 34) * 0.2).toFixed(1), // in mm/hour
+    uv: inputBuffer.readUInt8(offset + 43), // UV index
+    solarRadiation: inputBuffer.readUInt16LE(offset + 44), // in W/m2
+    last15MinRain: (inputBuffer.readUInt16LE(offset + 52) * 0.2).toFixed(1), // in mm
+    lastHourRain: (inputBuffer.readUInt16LE(offset + 54) * 0.2).toFixed(1), // in mm
+    last24HourRain: (inputBuffer.readUInt16LE(offset + 58) * 0.2).toFixed(1), // in mm
     timestamp: new Date().toISOString(),
   };
-  data.temperatureF = (data.temperatureRaw / 10).toFixed(1); // Temperature in degrees F
-  data.temperatureC = ((data.temperatureRaw / 10 - 32) * (5 / 9)).toFixed(1); // Temperature in degrees C
+  data.temperatureC = ((parseFloat(data.temperatureF) - 32) * (5 / 9)).toFixed(
+    1
+  ); // Temperature in degrees C
   data.disregardReason = "";
   if (data.windDirection < 1 || data.windDirection > 360) {
-    logger.log("warn", "Wind direction out of range", data);
     data.disregardReason = "Wind direction out of range";
+    logger.log("warn", "Wind direction out of range", data);
   } else if (data.windSpeed == 255 && data.humidity == 255) {
+    data.disregardReason =
+      "Wind speed and humidity are 255 - console is in setup mode";
     logger.log(
       "warn",
       "Wind speed and humidity are 255 - console is in setup mode"
     );
-    data.disregardReason =
-      "Wind speed and humidity are 255 - console is in setup mode";
   } else if (
     data.windSpeed > 150 ||
     data.temperatureC > 60 ||
     data.humidity > 100
   ) {
+    data.disregardReason = "Wind speed/temperature/humidity are too high";
     logger.log(
       "warn",
       "Data is weird - wind speed/temperature/humidity are too high"
     );
-    data.disregardReason = "Wind speed/temperature/humidity are too high";
   }
   return data;
 };
--- src/validator.js
+++ src/validator.js
@@ -1,31 +1,45 @@
-const logger = require("./logger")
+const logger = require("./logger");
 /**
  * Script to validate a buffer is of the format "loop"
  * @param {Buffer} inputBuffer
  */
 const validator = (inputBuffer) => {
-  let offset = false
-  const inputBufferLength = inputBuffer.length
+  let offset = false;
+  const inputBufferLength = inputBuffer.length;
   if (inputBufferLength < 40) {
-    logger.log("debug",`Buffer received too short - length is ${inputBufferLength}`,inputBuffer)
-    return false
+    logger.log(
+      "debug",
+      `Buffer received too short - length is ${inputBufferLength}`,
+      inputBuffer
+    );
+    return false;
   }
-  for (let thisOffset = 0; thisOffset < (inputBufferLength-3); thisOffset++) {
-    if (inputBuffer.slice(thisOffset,thisOffset+3).toString('utf8') === "LOO") {
-      offset = thisOffset
-      break
+  for (let thisOffset = 0; thisOffset < inputBufferLength - 3; thisOffset++) {
+    if (
+      inputBuffer.slice(thisOffset, thisOffset + 3).toString("utf8") === "LOO"
+    ) {
+      offset = thisOffset;
+      break;
     }
   }
-  if (!offset) { 
-    logger.log("debug","Buffer received doesn't contain LOO (76,79,79)",inputBuffer)
-    return false
+  if (!offset) {
+    logger.log(
+      "debug",
+      "Buffer received doesn't contain LOO (76,79,79)",
+      inputBuffer
+    );
+    return false;
   }
-  if ((inputBufferLength-offset) < 40) {
-    logger.log("debug",`Buffer received too short - length is ${inputBufferLength} and offset is ${offset} so there is not 40 after the offset`,inputBuffer)
-    return false
+  if (inputBufferLength - offset < 60) {
+    logger.log(
+      "debug",
+      `Buffer received too short - length is ${inputBufferLength} and offset is ${offset} so there is not 60 after the offset`,
+      inputBuffer
+    );
+    return false;
   }
 
-  logger.log("debug",`This offset is ${offset}`)
-  return offset
-}
-module.exports = validator
+  logger.log("debug", `This offset is ${offset}`);
+  return offset;
+};
+module.exports = validator;
